6ef50b3b1d6323f40e44e507ff264364d46b145c
Ganesh Sanjay Margale <ganeshmargale026@gmail.com>
2024-12-06 19:29:36 +0530

    Changes Routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,11 +4,10 @@ const eventController = require("../controllers/eventController");
 const router = express.Router();
 
 router.route("/create").post(eventController.createEvent);
-
 router.route("/register").post(eventController.participantRegister);
-
 router.route("/eventDetails/:id").get(eventController.getEvent);
-
 router.route("/allEvents").get(eventController.getAllEvents);
+router.route("/updateEvent/:id").patch(eventController.updateEventDetails);
+router.route("/deleteEvent/:id").delete(eventController.deleteEvent);
 
 module.exports = router;
